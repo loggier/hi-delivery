0d4128eeeaceed6acc440081903149ae60a9546d
Tracker Parser <info@trackerparser.com>
2025-10-16 05:44:58 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/components/data-table/data-table-toolbar.tsx
+++ src/components/data-table/data-table-toolbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Cross2Icon } from "@radix-ui/react-icons";
+import { X } from "lucide-react";
 import { type Table } from "@tanstack/react-table";
 import { SlidersHorizontal } from "lucide-react";
 
@@ -48,7 +48,7 @@ export function DataTableToolbar<TData>({
             className="h-8 px-2 lg:px-3"
           >
             Reiniciar
-            <Cross2Icon className="ml-2 h-4 w-4" />
+            <X className="ml-2 h-4 w-4" />
           </Button>
         )}
       </div>
