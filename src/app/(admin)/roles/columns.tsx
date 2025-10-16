04385111f1dd6a4af5835de97a7072bdaa70833f
Tracker Parser <info@trackerparser.com>
2025-10-16 05:20:04 +0000

    Try fixing this error: `Runtime Error: Error: Invalid time value. Error

--- src/app/(admin)/roles/columns.tsx
+++ src/app/(admin)/roles/columns.tsx
@@ -61,12 +61,12 @@ export const columns: ColumnDef<Role>[] = [
     },
   },
   {
-    accessorKey: "createdAt",
+    accessorKey: "created_at",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Creado en" />
     ),
     cell: ({ row }) => {
-      const date = new Date(row.getValue("createdAt"));
+      const date = new Date(row.getValue("created_at"));
       return <span>{format(date, "d 'de' MMMM, yyyy", { locale: es })}</span>;
     },
   },
