7442033e73459006d666141692112203080f5c30
Tracker Parser <info@trackerparser.com>
2025-11-13 00:56:32 +0000

    las etiquetas puedes ponerla en españor, year y month

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,6 +58,11 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
+      labels={{
+        ...props.labels,
+        labelMonthDropdown: () => "Mes",
+        labelYearDropdown: () => "Año",
+      }}
       {...props}
     />
   )
