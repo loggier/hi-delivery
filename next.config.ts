172d3162649c78f325989629045ad37988cfa816
Tracker Parser <info@trackerparser.com>
2025-11-18 15:58:56 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.rutasegura.vemontech.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
