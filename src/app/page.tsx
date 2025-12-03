276a70373bc6d27e7e4b2311204fd68f4829cfc6
Tracker Parser <info@trackerparser.com>
2025-12-03 06:42:17 +0000

    ppues algo esta mal porque / sigue llevando a dashboard, revisa por favo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import { Hero } from "./(site)/_components/hero";
-import { Benefits } from "./(site)/_components/benefits";
-import { HowItWorks } from "./(site)/_components/how-it-works";
-import { Requirements } from "./(site)/_components/requirements";
-import { ForBusinesses } from "./(site)/_components/for-businesses";
-import { Testimonials } from "./(site)/_components/testimonials";
-import { Faq } from "./(site)/_components/faq";
+import { redirect } from 'next/navigation';
 
-export default function SitePage() {
-  return (
-    <>
-      <Hero />
-      <Benefits />
-      <HowItWorks />
-      <Requirements />
-      <ForBusinesses />
-      <Testimonials />
-      <Faq />
-    </>
-  );
+export default function RootPage() {
+  redirect('/site');
 }
